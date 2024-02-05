651773038a146c17b76861797380a7936900b215
jpclemente97 <67484675+jpclemente97@users.noreply.github.com>
2024-02-05 14:28:36 +0100

    Delete _site directory

--- _site/get_publications.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -q http://nime-conference.github.io/NIME-bibliography/nime_papers.yaml -O ./_data/nime_papers.yaml
-wget -q http://nime-conference.github.io/NIME-bibliography/nime_music.yaml -O ./_data/nime_music.yaml
-wget -q http://nime-conference.github.io/NIME-bibliography/nime_installations.yaml -O ./_data/nime_installations.yaml
